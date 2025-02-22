043990be66af83802c40d7506f23044e4f1e32a0
shao <shaokang8069@gmail.com>
2025-02-22 23:54:52 +0800

    modified habit schema to include userId

--- server/src/models/Habit.js
+++ server/src/models/Habit.js
@@ -1,8 +1,10 @@
 import mongoose from "mongoose";
 
 const HabitSchema = new mongoose.Schema({
+	userId: { type: String, required: true }, // Associates habits with a user
 	name: { type: String, required: true },
 	streak: { type: Number, default: 0 },
+	lastCompleted: { type: Date, default: null },
 });
 
 export default mongoose.model("Habit", HabitSchema);
