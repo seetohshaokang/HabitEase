fdd95b3f186288743e0147aff3ba7aa7138e19f0
shao <shaokang8069@gmail.com>
2025-02-23 00:07:39 +0800

    added API endpoint for token generation

--- server/src/app.js
+++ server/src/app.js
@@ -6,6 +6,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Authentication route for generating JWT tokens
+app.use("/api/auth", authRoutes);
+
+// Habit tracking APi routes (secured with JWT)
 app.use("/api/habits", habitRoutes);
 
 export default app;
